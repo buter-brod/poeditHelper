04aa7db5eef5b14f5aba62502d9313a051b42209
buter-brod <mira.abash.nov@gmail.com>
2020-03-07 19:23:52 +0300

    added diff functionality

--- poeditHelper.py
+++ poeditHelper.py
@@ -1,20 +1,53 @@
 import sys
 import re
 
-rQuotes = re.compile("\\\"(.*?)\\\"")
-msgIdToken = "msgid"
-msgStrToken = "msgstr"
-
 class Entry:
     def __init__(self):
         self.key = ""
         self.trans = ""
 
-file1Entries = []
-file2Entries = []
+def parseEntries(fileStr):
+    
+    entries = []
+    entry = None
+    msgIdCapture = False
+    file1Lines = fileStr.splitlines()
+
+    for line in file1Lines:
+
+        if len(line) == 0 or line[0] == "~": continue
+
+        iskey1stLine = (line.find(msgIdToken) == 0)
+        istrans1stLine = (line.find(msgStrToken) == 0)
+
+        if iskey1stLine: 
+        
+            if entry and len(entry.key) > 0 : entries.append(entry)
+        
+            entry = Entry()
+            msgIdCapture = True
+
+        elif istrans1stLine:
+            msgIdCapture = False
+
+        m = rQuotes.match(line)
+        strInQuotes = m.group(1) if m else ""
+
+        if len(strInQuotes) > 0:
+            if msgIdCapture:
+                entry.key = entry.key + strInQuotes
+            else:
+                entry.trans = entry.trans + strInQuotes
+
+    return entries
+
 
 filename1 = sys.argv[1]
-filename2 = sys.argv[2] if len(sys.argv) > 3 else ""
+filename2 = sys.argv[2] if len(sys.argv) > 2 else ""
+
+rQuotes = re.compile(".*?\\\"(.*?)\\\"")
+msgIdToken = "msgid"
+msgStrToken = "msgstr"
 
 file1 = open(filename1, 'r', encoding='utf8')
 file2 = None
@@ -23,83 +56,85 @@ if filename2: file2 = open(filename2, 'r', encoding='utf8')
 file1Str = file1.read()
 file2Str = file2.read() if file2 else ""
 
-file1Lines = file1Str.splitlines()
+file1Entries = parseEntries(file1Str)
+file2Entries = parseEntries(file2Str) if file2Str and len(file2Str) > 0 else []
 
-entry = None
+def mainReport(entries):
+    rusTranslatedLen = 0
+    rusNonTranslatedLen = 0
 
-msgIdCapture = False
+    translatedList = []
+    nonTranslatedList = []
 
-for line in file1Lines:
+    for entry in file1Entries:
+        (translatedList if len(entry.trans) > 0 else nonTranslatedList).append(entry)
 
-    if len(line) == 0 or line[0] == "~": continue
+    rusTranslatedList = [entry.key for entry in translatedList]
+    rusNonTranslatedList = [entry.key for entry in nonTranslatedList]
 
-    iskey1stLine = (line.find(msgIdToken) == 0)
-    istrans1stLine = (line.find(msgStrToken) == 0)
+    allRusTextList  = [entry.key for entry in file1Entries]
+    allEngTextList  = [entry.trans for entry in file1Entries]
 
-    if iskey1stLine: 
-        
-        if entry and len(entry.key) > 0 : file1Entries.append(entry)
-        
-        entry = Entry()
-        msgIdCapture = True
+    allRusText = ' '.join(allRusTextList)
+    allEngText = ' '.join(allEngTextList)
 
-    elif istrans1stLine:
-        msgIdCapture = False
+    translatedRusText = ' '.join(rusTranslatedList)
+    untranslatedRusText = ' '.join(rusNonTranslatedList)
 
-    m = rQuotes.match(line)
-    strInQuotes = m.group(1) if m else ""
+    rusTranslatedLen = len(translatedRusText)
+    rusNonTranslatedLen = len(untranslatedRusText)
 
-    if len(strInQuotes) > 0:
-        if msgIdCapture:
-            entry.key = entry.key + strInQuotes
-        else:
-            entry.trans = entry.trans + strInQuotes
+    allRusLen = len(allRusText)
+    allEngLen = len(allEngText)
+
+    outputStr = ""
+
+    outputStr += "all russian text length: " + str(allRusLen) + "\n"
+    outputStr += "all english text length: " + str(allEngLen) + "\n"
+
+    outputStr += "russian text, that is already translated, length: " + str(rusTranslatedLen) + "\n"
+    outputStr += "russian text, that isn't translated yet, length: " + str(rusNonTranslatedLen) + "\n"
+
+    outputStr += "All russian text: \n" + allRusText
+    outputStr += "\n--------------------\nAll english text: \n--------------------\n" + allEngText
+    outputStr += "\n--------------------\nAll untranslated russian text: \n--------------------\n" + untranslatedRusText
+
+    return outputStr
+
+def diffReport(entries1, entries2):
+
+    dict1 = {}
+    dict2 = {}
+
+    for entry in entries1:
+        dict1[entry.key] = entry
+
+    for entry in entries2:
+        dict2[entry.key] = entry
+
+    entriesThatDiffer = []
+
+    for entry in entries2:
+
+        if entry.key not in dict1: return "error, entry '" + entry.key + "' not found in 1st file"
+
+        corrEntry = dict1[entry.key]
+
+        if corrEntry.trans != entry.trans:
+            entriesThatDiffer.append(entry)
+
+    allDiffersRusTextList  = [entry.key for entry in entriesThatDiffer]
+    allDiffersRusText = ' '.join(allDiffersRusTextList)
+    allDiffersRusTextLen = len(allDiffersRusText)
+
+    outputStr = ""
+    outputStr += "russian text, translation for which differs, length: " + str(allDiffersRusTextLen) + "\n"
+    outputStr += "Text itself: \n" + allDiffersRusText
+
+    return outputStr
 
 
-rusTranslatedLen = 0
-rusNonTranslatedLen = 0
-
-translatedList = []
-nonTranslatedList = []
-
-for entry in file1Entries:
-
-    (translatedList if len(entry.trans) > 0 else nonTranslatedList).append(entry)
-
-    #if len(entry.trans) > 0:
-     #   translatedList.append(entry)
-    #else:
-    #    nonTranslatedList.append(entry)
-
-rusTranslatedList = [entry.key for entry in translatedList]
-rusNonTranslatedList = [entry.key for entry in nonTranslatedList]
-
-allRusTextList  = [entry.key for entry in file1Entries]
-allEngTextList  = [entry.trans for entry in file1Entries]
-
-allRusText = ' '.join(allRusTextList)
-allEngText = ' '.join(allEngTextList)
-
-translatedRusText = ' '.join(rusTranslatedList)
-untranslatedRusText = ' '.join(rusNonTranslatedList)
-
-rusTranslatedLen = len(translatedRusText)
-rusNonTranslatedLen = len(untranslatedRusText)
-
-allRusLen = len(allRusText)
-allEngLen = len(allEngText)
-
-outputStr = ""
-
-outputStr += "all russian text length: " + str(allRusLen) + "\n"
-outputStr += "all english text length: " + str(allEngLen) + "\n"
-
-outputStr += "russian text, that is already translated, length: " + str(rusTranslatedLen) + "\n"
-outputStr += "russian text, that isn't translated yet, length: " + str(rusNonTranslatedLen) + "\n"
-
-outputStr += "All russian text: \n" + allRusText
-outputStr += "\n--------------------\nAll english text: \n--------------------\n" + allEngText
-outputStr += "\n--------------------\nAll untranslated russian text: \n--------------------\n" + untranslatedRusText
+outputStr = mainReport(file1Entries) if len(file2Entries) == 0 else diffReport(file1Entries, file2Entries)
 
 print(outputStr)
 
@@ -108,8 +143,4 @@ output.write(outputStr)
 output.close()
 
 input("Press Enter to exit...")
-
-
-
-
 
